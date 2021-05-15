f681d47ee4dc06de78c498c011d8f9ebc66a33e6
sarthakvohra <56431434+sarthakvohra@users.noreply.github.com>
2021-05-15 10:43:09 +0530

    everything works

--- app.js
+++ app.js
@@ -5,39 +5,71 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 app.listen(3000);
 app.set("view engine", "ejs");
-var data = [],
+var data,
   i = 0;
 // app.use(bodyParser.json()); // for parsing application/json
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 
+// function abcd(districtID, date) {
+//   url =
+//     "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=" +
+//     districtID +
+//     "&date=" +
+//     date;
+//   console.log(url);
+//   var request = new XMLHttpRequest();
+//   request.open("GET", url, true);
+//   var centres;
+//   request.onload = function () {
+//     if (request.status >= 200 && request.status < 400) {
+//       // Success!
+//       data[0] = JSON.parse(request.responseText); //bug over here
+//       // centres = data[i]["sessions"];
+//       i++;
+//       // console.log(centres);
+//     } else {
+//       // We reached our target server, but it returned an error
+//     }
+//   };
+//   request.onerror = function () {
+//     // There was a connection error of some sort
+//   };
+//   request.send();
+//   return data;
+// }
+
 function abcd(districtID, date) {
-  url =
-    "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=" +
-    districtID +
-    "&date=" +
-    date;
-  console.log(url);
-  var request = new XMLHttpRequest();
-  request.open("GET", url, true);
-  var centres;
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-      // Success!
-      data[i] = JSON.parse(request.responseText); //bug over here
-      // centres = data[i]["sessions"];
-      i++;
-      // console.log(centres);
-    } else {
-      // We reached our target server, but it returned an error
-    }
-  };
-  request.onerror = function () {
-    // There was a connection error of some sort
-  };
-  request.send();
-  return data;
+  //aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+  return new Promise((resolve, reject) => {
+    url =
+      "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=" +
+      districtID +
+      "&date=" +
+      date;
+    console.log(url);
+    const request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    var centres;
+    request.onload = function () {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        data = JSON.parse(request.responseText); //bug over here
+        resolve(data);
+        // centres = data[i]["sessions"];
+        i++;
+        // console.log(centres);
+      } else {
+        // We reached our target server, but it returned an error
+      }
+    };
+    request.onerror = function () {
+      // There was a connection error of some sort
+    };
+    request.send();
+    // return data;
+  });
 }
 var districtID;
 
@@ -49,17 +81,24 @@ if (dd < 10) dd = "0" + dd;
 if (mm < 10) mm = "0" + mm;
 date = dd + "-" + mm + "-" + yyyy;
 // centres = abcd("149", date);
-centres = abcd("149", "24-05-2021"); //works with this
+// centres = abcd("149", "24-05-2021"); //works with this
 // centres = abcd(districtID, "24-05-2021");    ///doesn't work with it
 
 // console.log(centres);
-app.get("/:id", (req, res) => {
+app.get("/show/:id", (req, res) => {
   const id = req.params.id;
   districtID = String(id);
   // console.log(id);
   // centres = abcd("149", "24-05-2021");
-  // centres = abcd(districtID, "24-05-2021");
-  res.render("index", { centres: centres[-1]["centers"] });
+  // centres = abcd(districtID, "24-05-2021");//works
+  abcd(districtID, "24-05-2021")
+    .then((data) => {
+      // console.log("aaa");
+      res.render("index", { centres: data["centers"] });
+    })
+    .catch((err) => console.log(err));
+  // console.log(centres[0]["centers"]);
+  // res.render("index", { centres: centres[0]["centers"] });
 });
 
 app.get("/form", (req, res) => res.render("getDistrict"));
@@ -68,6 +107,6 @@ app.post("/form", (req, res) => {
   // const { districtID, districtName, date } = req.body;
   const { districtID, date } = req.body;
   console.log(districtID, date);
-  res.json({ redirect: "/" + districtID });
+  res.json({ redirect: "/show/" + districtID });
   // res.redirect("/");
 });
