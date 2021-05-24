45cae69510b81e4e3e326d876abf95eef2ad1595
sarthakvohra <56431434+sarthakvohra@users.noreply.github.com>
2021-05-24 18:10:55 +0530

    Added some functionality

--- Cowin2_Shimla.py
+++ Cowin2_Shimla.py
@@ -16,42 +16,38 @@ def notInblacklist(name):
             return False
     return True
 
-def get_data(districtCode, date, vaccine="*"):
+def condition(ses, vaccine, age):
+    if vaccine=="*" and age==0:
+        return ses["available_capacity"]>1
+    if vaccine!="*" and age==0:
+        return ses["available_capacity"]>1 and ses["vaccine"]==vaccine
+    if vaccine=="*" and age!=0:
+        return ses["available_capacity"]>1 and ses["min_age_limit"]==age
+    if vaccine!="*" and age!=0:
+        return ses["available_capacity"]>1 and ses["vaccine"]==vaccine and ses["min_age_limit"]==age
+
+
+def get_data(districtCode, date, vaccine="*", age=0):
     r=requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id="+districtCode+"&date="+date,headers=headers)
     data=r.json()
     check=r.text
     centres=data["centers"]
-        # centers=ses["name"]
     if (len(centres)>0):
         totalCentres=0
         for centre in centres:
             if notInblacklist(centre["name"]):
                 ctr=0
                 sessions = centre["sessions"]
-                if vaccine!="*":
-                    for ses in sessions:
-                        if ses["available_capacity"]>0 and ses["vaccine"]==vaccine:
-                            # data_all+=f'Date: {ses["date"]}\nVaccine: {ses["vaccine"]}\nMin Age: {ses["min_age_limit"]}\nCapacity: {ses["available_capacity"]}\n'
-                            ctr+=1
-                            totalCentres+=1
-                    if ctr!=0:
-                        print(f'\nCentre name: {centre["name"]}\nCentre address: {centre["address"]}')
-                        sessions = centre["sessions"]
-                    for ses in sessions:
-                        if ses["available_capacity"]>0 and ses["vaccine"]==vaccine:
-                            print(f'Date: {ses["date"]}\nVaccine: {ses["vaccine"]}\nMin Age: {ses["min_age_limit"]}\nCapacity: {ses["available_capacity"]}')
-                else:
-                    for ses in sessions:
-                        if ses["available_capacity"]>0:
-                            ctr+=1
-                            totalCentres+=1
-                    if ctr!=0:
-                        print(f'\nCentre name: {centre["name"]}\nCentre address: {centre["address"]}')
-                        sessions = centre["sessions"]
-                    for ses in sessions:
-                        if ses["available_capacity"]>0:
-                            print(f'Date: {ses["date"]}\nVaccine: {ses["vaccine"]}\nMin Age: {ses["min_age_limit"]}\nCapacity: {ses["available_capacity"]}')
-        # 	# data_all.append({"centre_name":centre["name"],"centre_address":centre["address"], "vaccine":centre["vaccine"],"minAge": centre["min_age_limit"]})
+                for ses in sessions:
+                    if condition(ses, vaccine, age):
+                        ctr+=1
+                        totalCentres+=1
+                if ctr!=0:
+                    print(f'\nCentre name: {centre["name"]}\nCentre address: {centre["address"]}')
+                    sessions = centre["sessions"]
+                for ses in sessions:
+                    if condition(ses, vaccine, age):
+                        print(f'Date: {ses["date"]}\nVaccine: {ses["vaccine"]}\nMin Age: {ses["min_age_limit"]}\nCapacity: {ses["available_capacity"]}')
         if totalCentres==0:
             return False
         return True
@@ -68,7 +64,7 @@ while True:       #to let the program refresh every minute
         x+=datetime.timedelta(days=i*7)
         date=x.strftime("%d-%m-%Y")
         # if get_data("149", date)==False:    #for south delhi
-        if get_data("208", date)==False:      #for gurgaon 208-shimla
+        if get_data("208", date, "*", 18)==False:      #for gurgaon 208-shimla
             # print(f'Week {i+1}: No centre found')
             ctr+=1
     if ctr==4:
